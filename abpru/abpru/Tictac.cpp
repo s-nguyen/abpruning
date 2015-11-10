2d7c64ec0d43dc2f73bcffd742e7cb34f31ab561
Stephen Nguyen <nereuxie@gmail.com>
2015-11-10 13:56:20 -0700

    added classes

---
+++ abpru/abpru/Tictac.cpp
@@ -0,0 +1,17 @@
+#include "Tictac.h"
+
+
+
+Tictac::Tictac()
+{
+}
+
+
+Tictac::~Tictac()
+{
+}
+
+void Tictac::ReadInput(char* s, std::vector<std::string> v)
+{
+
+}
---
+++ abpru/abpru/Tictac.h
@@ -0,0 +1,11 @@
+#pragma once
+#include  <vector>
+#include <string>
+class Tictac
+{
+public:
+	Tictac();
+	~Tictac();
+	void ReadInput(char* s, std::vector<std::string> v);
+};
+
